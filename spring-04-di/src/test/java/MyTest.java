53cbe045273f16bd0d4ab85ed1538b829af342ed
danger.zeng <danger.zeng@qq.com>
2021-03-22 16:54:23 +0800

    spring-04-di:P命令注入和C命令注入

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -1,6 +1,8 @@
 
 import com.zeng.pojo.Address;
 import com.zeng.pojo.Student;
+import com.zeng.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,6 +12,12 @@ public class MyTest {
         Student student = (Student) context.getBean("student");
         System.out.println(student.toString());
     }
+@Test
+    public void Test(){
+    ApplicationContext context=new ClassPathXmlApplicationContext("userbeans.xml");
+    User user = (User) context.getBean("user2");
+    System.out.println(user.toString());
+}
 
 
 }
