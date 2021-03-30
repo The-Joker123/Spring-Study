c17a2bcc131aff727c9bd6d87df523e3b3a4beb3
The-Joker123 <danger.zeng@qq.com>
2021-03-30 20:54:26 +0800

    spring-08-proxy:demo03注释

--- spring-08-proxy/src/main/java/com/zeng/demo03/ProxyInvocationHandler.java
+++ spring-08-proxy/src/main/java/com/zeng/demo03/ProxyInvocationHandler.java
@@ -12,11 +12,11 @@ public class ProxyInvocationHandler implements InvocationHandler {
         this.subject = subject;
     }
 
-
+//生成代理类，重点是第二个参数，获取要代理的抽象角色！之前都是一个角色，现在可以代理一类角色
     public Object getProxy(){
         return Proxy.newProxyInstance(this.getClass().getClassLoader(),subject.getClass().getInterfaces(),this);
     }
-
+//处理代理实例上的方法调用并返回结果
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object result =method.invoke(subject,args);
         return null;
--- spring-08-proxy/src/main/java/com/zeng/demo03/Test.java
+++ spring-08-proxy/src/main/java/com/zeng/demo03/Test.java
@@ -2,10 +2,13 @@ package com.zeng.demo03;
 
 public class Test {
     public static void main(String[] args) {
+        //真实角色
         RealSubject realSubject=new RealSubject();
+        //代理实例的调用处理程序
          ProxyInvocationHandler pih =new ProxyInvocationHandler();
-         pih.setSubject(realSubject);
-         Subject proxy=(Subject)pih.getProxy();
+         pih.setSubject(realSubject);//将真实角色放置进去！
+         Subject proxy=(Subject)pih.getProxy();//动态生产对应的代理类！
          proxy.Request();
+         //动态代理就是代理接口
     }
 }
