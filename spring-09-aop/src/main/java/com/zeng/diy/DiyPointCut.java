899826d257c83a6330c5c3608fff7c5d4eb90920
The-Joker123 <danger.zeng@qq.com>
2021-03-31 21:58:04 +0800

    spring-09-aop:aop第2种方法

---
+++ spring-09-aop/src/main/java/com/zeng/diy/DiyPointCut.java
@@ -0,0 +1,10 @@
+package com.zeng.diy;
+
+public class DiyPointCut {
+    public void before(){
+        System.out.println("======方法执行前");
+    }
+    public void after(){
+        System.out.println("======方法执行后");
+    }
+}
